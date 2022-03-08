678411dd0b9d7a8380cf9e493db39cf5dae4ce66
Jonathan Yantis <yantis@quicksortrx.com>
2022-03-08 17:06:26 -0500

    Remove quite mode (default)

--- extra/ads-db.sh
+++ extra/ads-db.sh
@@ -2,4 +2,4 @@
 # dump1090exporter --port=9105   --latitude=32.78   --longitude=-79.9419   --debug --url=http://127.0.0.1/dump1090 >> /tmp/dump-exporter.log 2>> /tmp/dump-exporter.log&
 cd /home/pi/ads-db
 source venv/bin/activate
-./ads-db.py --quiet -D -sc 50 -rs 127.0.0.1
+./ads-db.py -D -sc 50 -rs 127.0.0.1
